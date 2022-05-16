3ded012a3bf86b07a94345aab2353172bbee0853
Minh.N.Pham <minhmuofficial@gmail.com>
2022-05-16 19:40:28 +0700

    [add] adding setup scripts

--- includes/setup.php
+++ includes/setup.php
@@ -15,21 +15,23 @@ wp_enqueue_script('cryptopay-main', CRYPTOPAY_URL . 'assets/js/crypto-main.js', 
 if (is_wc_endpoint_url('order-pay')) {
     wp_enqueue_script('metamask', INVEST_URL . 'assets/js/metamask.js', array('jquery'), null, true);
     $order          = wc_get_order(get_query_var('order-pay'));
-    $payment_status = $order->get_status();
-    $user_wallet    = get_field('_owner_address', $order->id);
-    $extradata = array(
-        'ajaxurl'           => admin_url('admin-ajax.php'),
-        'nonce'             => wp_create_nonce('crypto-pay-nonce'),
-        'url'               => CRYPTOPAY_URL,
-        'receiving_wallet'  => get_field('_receiving_wallet', 'option'),
-        'amount'            => $order->get_total(),
-        'order_id'          => $order->id,
-        'order_status'      => $payment_status,
-        'order_key'         => $order->get_order_key(),
-        'exchange_rate'     => get_field('_exchange_rate', 'option') ?? "1",
-        'user_wallet'       => $user_wallet,
-        'master_abi'        => get_field('_withdraw_master_abi', 'option') ?? [],
-        'contract_address'  => get_field('_withdraw_contract_address', 'option') ?? "",
-    );
-    wp_localize_script('cryptopay-main', 'extraCryptoPay', $extradata);
+    if ($order) {
+        $payment_status = $order->get_status();
+        $user_wallet    = get_field('_owner_address', $order->id);
+        $extradata = array(
+            'ajaxurl'           => admin_url('admin-ajax.php'),
+            'nonce'             => wp_create_nonce('crypto-pay-nonce'),
+            'url'               => CRYPTOPAY_URL,
+            'receiving_wallet'  => get_field('_receiving_wallet', 'option'),
+            'amount'            => $order->get_total(),
+            'order_id'          => $order->id,
+            'order_status'      => $payment_status,
+            'order_key'         => $order->get_order_key(),
+            'exchange_rate'     => get_field('_exchange_rate', 'option') ?? "1",
+            'user_wallet'       => $user_wallet,
+            'master_abi'        => get_field('_withdraw_master_abi', 'option') ?? [],
+            'contract_address'  => get_field('_withdraw_contract_address', 'option') ?? "",
+        );
+        wp_localize_script('cryptopay-main', 'extraCryptoPay', $extradata);
+    }
 }
